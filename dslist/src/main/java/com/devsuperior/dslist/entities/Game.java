9ec410dfa68b03faf0f5a6be67b3b941446d488f
Gilson Silva <gbatistas@yahoo.com.br>
2025-01-26 20:04:44 -0300

    Game Seed

--- dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity /*Configura a classe java p/ que seja equivalente a uma tabela do banco relacional */
-@Table /* Custominar o nome da tabela no banco */
+@Table(name = "tb_game") /* Custominar o nome da tabela no banco */
 public class Game {
 
 	@Id /* Define o Id da minha tabela*/
@@ -21,22 +21,27 @@ public class Game {
 	@Column(name = "game_year")  /* custominar o nome da coluna na tabela do banco, já que o nome year é uma palavra do sql.*/
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT") /* Define que a minha coluna na tabela será TEXT, se não colocar essa anotação vai ser criado como varcahr(255)*/
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT") /* Define que a minha coluna na tabela será TEXT, se não colocar essa anotação vai ser criado como varcahr(255)*/
 	private String longDescription;
 	
 	public Game() {
 	}  /* Construtor sem argumento */
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		/*super(); Pode apagar esse */
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription; /* esse tipo camelcase no banco vai ficar com short_Description*/
 		this.longDescription = longDescription; /* esse tipo camelcase no banco vai ficar com long_Description*/
@@ -74,12 +79,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
